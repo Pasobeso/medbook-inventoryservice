2e4e344cd573b943581015afefac58fce9384373
Ittiwat Chuchoet <ciaabcdefg@gmail.com>
2025-10-19 17:25:04 +0700

    refactor init logic & migrations

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 use anyhow::Result;
 use axum::Router;
-use medbook_core::bootstrap::bootstrap;
+use diesel_migrations::{EmbeddedMigrations, embed_migrations};
+use medbook_core::{
+    bootstrap::{self, bootstrap},
+    db,
+};
 use medbook_inventoryservice::{consumers, routes};
 use tower_http::services::ServeDir;
 
+/// Migrations embedded into the binary which helps with streamlining image building process
+const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    bootstrap::init_tracing();
+    bootstrap::init_env();
+
     let static_service = ServeDir::new("./assets");
     let app = Router::new()
         .merge(routes::products::routes())
         .nest_service("/assets", static_service);
+
+    tracing::info!("Running migrations...");
+    let migrations_count =
+        db::run_migrations_blocking(MIGRATIONS, &std::env::var("DATABASE_URL")?).await?;
+    tracing::info!("Run {} new migrations successfully", migrations_count);
 
     bootstrap(
         "InventoryService",
